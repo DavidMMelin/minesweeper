3811e9466861032780fbeec8a7cb6a475eb88c36
David + Belda <David&Belda@appacademy.io>
2013-05-20 16:19:40 -0700

    Added bomb counter to answer board

--- minesweeper.rb
+++ minesweeper.rb
@@ -1,10 +1,12 @@
 class Minesweeper
   attr_accessor :size, :answer_board, :user_board, :bomb_positions
+  SURROUNDING_CELLS = [[-1,-1], [-1,0], [-1,1], [0,-1], [0,1], [1,-1], [1,0], [1,1]]
 
   def initialize(size = 9)
     @size = size
     @total_bombs = (size == 9 ? 10 : 40)
     @answer_board = create_answer_board
+    tally_bombs
     @user_board = make_board
   end
 
@@ -12,18 +14,20 @@ class Minesweeper
     until winner?
       display_board(@user_board)
       get_move
+      # places move
     end
   end
 
-  def winner?
+  def winner? #compare flag positions with our bomb positions || count flags and positions
+    # flag_positions == bomb_positions
     @bomb_positions.each do |position|
       return false if @user_board[position[0]][position[1]] != 'F'
     end
     true
   end
 
-  def display_board(board)
-    board.each do |row|
+  def display_board(board) # change way info is stored in userboard, use that info to display
+    board.each do |row|    # ex: :flag = 'F'
       p row
     end
   end
@@ -32,12 +36,13 @@ class Minesweeper
     print "Enter row,column of your move: "
     move = gets.chomp.split(/,/).map! { |num| num.to_i }
 
-    until valid_move?(move)
+    until valid_move?(move) #add an invalid statement
+
       print "Enter row,column of your move: "
       move = gets.chomp.split(/,/).map! { |num| num.to_i }
     end
     print "Discover 'D' or flag 'F': "
-    action = gets.chomp
+    action = gets.chomp #translate action here
     place_move(move, action)
   end
 
@@ -51,27 +56,27 @@ class Minesweeper
     case @answer_board[row][column]
     when "*"
       if action == "D"
-        @user_board[row][column] = "_"
-      else
+        @user_board[row][column] = "_" #discover call
+      elsif action == "F"
         @user_board[row][column] = "F"
       end
     when "B"
       if action == "F"
         @user_board[row][column] = "F"
-      else
+      elsif action == "D"
         raise "YouBlewUpError"
+      end
+    else
+      if action == "F"
+        @user_board[row][column] = "F"
+      elsif action == "D"
+        @user_board[row][column] = @answer_board[row][column]
       end
     end
   end
 
-  def make_move
-
-    update_board(move)
-
-  end
-
   def make_board
-    board = Array.new(@size){Array.new(@size) {'*'} }
+    board = Array.new(@size) { Array.new(@size) { '*' } }
   end
 
   def create_answer_board
@@ -85,10 +90,47 @@ class Minesweeper
     end
 
     @bomb_positions.each do |bomb|
-      @answer_board[bomb[0]][bomb[-1]] = "B"
+      @answer_board[bomb[0]][bomb[1]] = "B"
     end
 
     @answer_board
+  end
+
+  # SURROUNDING_CELLS = [[-1,-1], [-1,0], [-1,1], [0,-1], [0,1], [1,-1], [1,0], [1,1]]
+
+  def tally_bombs
+    # each position in answer board
+      # skip if bomb
+      # get surrounding cells
+      # count the number of bombs in above
+      # put into answer key
+
+    @answer_board.each_with_index do |row, r_idx|
+      row.each_with_index do |value, c_idx|
+        next if @answer_board[r_idx][c_idx] == "B"
+        bomb_tally = 0
+        SURROUNDING_CELLS.each do |position|
+          neighbor_cell = [r_idx + position[0], c_idx + position[1]]
+          if valid_cell?(neighbor_cell)
+            bomb_tally += increase_bomb_tally(neighbor_cell)
+          end
+        end
+        @answer_board[r_idx][c_idx] = bomb_tally unless bomb_tally == 0
+      end
+    end
+
+  end
+
+  def increase_bomb_tally(neighbor_cell)
+    if @answer_board[neighbor_cell[0]][neighbor_cell[1]] == 'B'
+      1
+    else
+      0
+    end
+  end
+
+  def valid_cell?(neighbor)
+    neighbor[0].between?(0,8) && neighbor[1].between?(0, 8)
   end
 
 end
